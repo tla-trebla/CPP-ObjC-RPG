2f2884941fb72d5cfb4404d0ba62f45495b0a108
Albert Pangestu <stamina.left@gmail.com>
2024-07-04 13:13:41 +0700

    Initial Commit

---
+++ CPP-RPG/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPP-RPG
+//
+//  Created by Albert Pangestu on 04/07/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
